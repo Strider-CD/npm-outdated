ad531b12032cddf898caa3bdaec6c2b27719291b
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-21 22:44:39 -0700

    give maintainer data

--- index.js
+++ index.js
@@ -20,25 +20,33 @@ client.fetchPlugins().then(function(plugins) {
   return new Promise(function(resolve, reject) {
     needle.get(plugin.registryURL, function(err, res) {
       if (err) return reject(err);
-      plugin.npm = res.body
       if (res.body.error) {
         if (! /version not found/.test(res.body.error))
           return reject(new Error(res.body.error))
-        plugin.outdated = true;
+        needle.get(registryURL(plugin.name, 'latest'), function(err, res) {
+          plugin.latestPublished = res.body.version;
+          plugin.maintainers = res.body.maintainers;
+          plugin.outdated = true;
+          resolve(plugin)
+        })
       } else {
-        //console.log(res.body.author, res.body.maintainers);
         plugin.outdated = false;
+        resolve(plugin)
       }
-      resolve(plugin)
     })
   });
 }).map(function(plugin) {
   var label = plugin.name+'@'+plugin.version
-    console.log(plugin.npm.maintainers)
-  if (plugin.outdated)
-    console.log(chalk.red('✘'), label, 'is NOT on npm', plugin.npm.maintainers);
-  else
-    console.log(chalk.green('✓'), label, 'is on npm');
+  if (plugin.outdated) {
+    console.log(chalk.red('✘'), label, 'is NOT on npm')
+    console.log('\t', 'npm version:', plugin.latestPublished)
+    console.log('\t', 'maintainers:')
+    plugin.maintainers.forEach(function(m) {
+      console.log('\t', chalk.yellow('=>'), m.name, '<'+m.email+'>')
+    })
+  } else {
+    console.log(chalk.green('✓'), chalk.grey(label, 'is on npm'))
+  }
 }).error(function(err) {
   console.error(err.stack);
 }).catch(function(err) {
